4022e5c74faf84ab6c0e5ad7e9c1fbfcba2fab53
zap-websaite <y.syuta2024@gmail.com>
2024-12-03 23:01:43 +0900

    Delete my_website-main directory

--- my_website-main/MY＿websaite/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("form");
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        alert("送信されました！");
-    });
-});
